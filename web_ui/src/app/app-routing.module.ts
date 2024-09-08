14577a22af18e09b8ef93840049ac4ea3c3ea02f
Matheus de Oliveira <matheus82pessoal@protonmail.com>
2024-09-08 11:36:07 -0300

    routing

--- web_ui/src/app/app-routing.module.ts
+++ web_ui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './src/app/home/components/article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: ArticleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
