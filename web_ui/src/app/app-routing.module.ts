83293f8863d573483742f4eed3c55c8bd7286d21
Matheus de Oliveira <matheus82pessoal@protonmail.com>
2024-09-08 14:47:21 -0300

    adding contact and about page. Stylizing home page

--- web_ui/src/app/app-routing.module.ts
+++ web_ui/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ArticleComponent } from './src/app/home/components/article/article.component';
+import { ArticleComponent } from './home/components/article/article.component';
+import { AboutMeComponent } from './about_me/components/about-me/about-me.component';
+import { ContactsComponent } from './contact/components/contacts/contacts.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: ArticleComponent},
+  {path: 'about', component: AboutMeComponent},
+  {path: 'contact', component: ContactsComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- web_ui/src/app/app.module.ts
+++ web_ui/src/app/app.module.ts
@@ -3,21 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArticleComponent } from './src/app/home/components/article/article.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
+import { ArticleComponent } from './home/components/article/article.component';
+import { AboutMeComponent } from './about_me/components/about-me/about-me.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { ContactsComponent } from './contact/components/contacts/contacts.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    ArticleComponent,
+    AboutMeComponent,
+    NavBarComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
-    MatChipsModule
+    MatChipsModule,
+    MatGridListModule,
+    MatToolbarModule
   ],
   providers: [
     provideAnimationsAsync()
